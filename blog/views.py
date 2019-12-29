ebd6bff5b3a2a774ad86fbe237087b667f236212
Nathan <nate.hilbrands@gmail.com>
2019-12-29 13:40:36 -0500

    revert to retry

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
-from .readpdfFUNCmain import globaldf
+import pyGithub
+
+#from .readpdfFUNCmain import globaldf
+
 # Create your views here.
 def post_list(request):
-    #posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    df = globaldf
-    return render(request, 'blog/post_list.html', {'posts': df})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    #df = globaldf
+    #df = 'hi'
+    return render(request, 'blog/post_list.html', {'posts': posts})
